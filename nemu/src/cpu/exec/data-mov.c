85803d6e91042a0bf20ad4fd846b230a152ba1da
tracer-ics2017 <tracer@njuics.org>
2017-08-26 15:51:36 +0800

    >  compile 151220100 tangruize Linux TRZ 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:51:36 up 22:31,  2 users,  load average: 0.07, 0.09, 0.13 c3d546a3408fe332137dfa5af1089b2f541ace36

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -60,7 +60,7 @@ make_EHelper(cwtl) {
 make_EHelper(movsx) {
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
   rtl_sext(&t2, &id_src->val, id_src->width);
-  operand_write(id_dest, &t2);
+  //operand_write(id_dest, &t2);
   print_asm_template2(movsx);
 }
 
