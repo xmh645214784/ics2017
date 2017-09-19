ba47cf9cd9a7fab2d0a85338bf068d8493be340e
tracer-ics2017 <tracer@njuics.org>
2017-09-19 18:36:11 +0800

    >  compile 151220100 tangruize Linux TRZ 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:36:11 up 7 days, 20:53,  1 user,  load average: 0.44, 0.37, 0.33 7ef2e3b05ba0d62a9c7aabd418050a0c406649e5

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -130,7 +130,7 @@ opcode_entry opcode_table [512] = {
   /* 0xdc */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xe0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xe4 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xe8 */	IDEX(I, call), EMPTY, EMPTY, IDEXW(SI, jmp, 1),
+  /* 0xe8 */	IDEX(I, call), IDEX(I, jmp), EMPTY, IDEXW(SI, jmp, 1),
   /* 0xec */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xf0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xf4 */	EMPTY, EMPTY, IDEXW(E, gp3, 1), IDEX(E, gp3),
