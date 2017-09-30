19aeaec4c6ae001662f841279e9144876639098d
tracer-ics2017 <tracer@njuics.org>
2017-09-30 20:23:16 +0800

    >  compile 151220100 tangruize Linux TRZ 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:23:16 up 1 day,  5:33,  1 user,  load average: 0.79, 0.55, 0.66 f239c298cafc60bf9c4d23e1c5415cb75dffaa03

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,4 +1,5 @@
 #include "nemu.h"
+#include "device/mmio.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -12,11 +13,18 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  int i = is_mmio(addr);
+  if (i == -1)
+    return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  return mmio_read(addr, len, i);
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+  int i = is_mmio(addr);
+  if (i == -1)
+    memcpy(guest_to_host(addr), &data, len);
+  else
+    mmio_write(addr, len, data, i);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
