85726d106a8607d3d6749002f2efaa414f2ba778
tracer-ics2017 <tracer@njuics.org>
2017-10-08 11:43:20 +0800

    >  compile 151220100 tangruize Linux TRZ 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  11:43:20 up  1:17,  1 user,  load average: 0.20, 0.30, 0.36 f5aaae7bc04e42ce38d2570a9094ac9bd7883bc

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,8 +5,8 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-
-  TODO();
+  printf("%s %d\n", "No: ", NO);
+  //TODO();
 
 }
 
