3f1f07e288a302f6f99fc2bb7d939ab41c50703e
tracer-ics2017 <tracer@njuics.org>
2017-11-24 21:46:23 +0800

    >  compile 151220100 tangruize Linux TRZ 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:46:23 up 12:01,  3 users,  load average: 0.46, 0.43, 0.37 31a23585c41085f7ec1ad82d7e8c4120c9762035

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -65,35 +65,24 @@ make_EHelper(leave) {
 
 make_EHelper(cltd) {
   if (decoding.is_operand_size_16) {
-    //TODO();
-    if (reg_w(R_EAX) < 0) {
-      reg_l(R_EDX) = 0x0000ffff;
-    }
-    else {
-      reg_l(R_EDX) = 0;
-    }
+    rtl_sext(&t0, &reg_l(R_EAX), 2);
+    rtl_shri(&reg_l(R_EDX), &t0, 16);
   }
   else {
-    //TODO();
-    if (reg_l(R_EAX) < 0) {
-      reg_l(R_EDX) = 0xffffffff;
-    }
-    else {
-      reg_l(R_EDX) = 0;
-    }
+    rtl_sari(&reg_l(R_EDX), &reg_l(R_EAX), 31);
   }
-
   print_asm(decoding.is_operand_size_16 ? "cwtl" : "cltd");
 }
 
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
-    TODO();
+    rtl_shli(&reg_l(R_EAX), &reg_l(R_EAX), 24);
+    rtl_sari(&reg_l(R_EAX), &reg_l(R_EAX), 8);
+    rtl_shri(&reg_l(R_EAX), &reg_l(R_EAX), 16);
   }
   else {
-    TODO();
-  }
-
+    rtl_sext(&reg_l(R_EAX), &reg_l(R_EAX), 2);
+  } 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
 }
 
