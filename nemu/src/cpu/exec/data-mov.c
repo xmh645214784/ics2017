14b2034e3275ba59b724ec39d201184b7b711c16
tracer-ics2017 <tracer@njuics.org>
2017-11-26 15:19:38 +0800

    >  compile 151220100 tangruize Linux TRZ 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:19:38 up  3:15,  4 users,  load average: 0.39, 0.36, 0.33 e15ada8b5461a19cce444007e341d31419274b5

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -108,13 +108,11 @@ make_EHelper(lea) {
 }
 
 make_EHelper(movcr_s) {
-  printf("movcr_s: %x, %x\n", cpu.CR[id_src->reg],id_dest->val );
   cpu.CR[id_src->reg] = id_dest->val;
-  print_asm("movcr_s");
+  print_asm("movl %s,cr%d", id_dest->str, id_src->reg);
 }
 
 make_EHelper(movcr_l) {
-  printf("movcr_l: %x, %x\n", reg_l(id_src->reg),cpu.CR[id_dest->reg] );
   reg_l(id_src->reg) = cpu.CR[id_dest->reg];
-  print_asm("movcr_l");
+  print_asm("movl cr%d,%s", id_dest->reg, id_src->str);
 }
